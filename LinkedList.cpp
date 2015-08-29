31bf95ed24f1ad1f439a3eaf839eee318c50bcdf
soachishti <soac@outlook.com>
2015-08-29 22:43:45 +0500

    No linkedlist delete all node with same key like first we have to delete 1 then it will delete every node with info 1

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -66,25 +66,30 @@ class LinkedList {
         tmp = head;
         tmp2 = NULL;
         do {
-            if (tmp->info == key) break;   
+            if (tmp->info == key) {
+                if (tmp && tmp2) {
+                    // Remove value in middle
+                    tmp2->next = tmp->next;
+                    delete tmp;
+                    tmp = tmp2;
+                }
+                else if(!tmp2) {
+                    // removing first value
+                    head = tmp->next;
+                    delete tmp;
+                    tmp = head;
+                }
+                else {
+                    // removing last value
+                    tmp2->next = NULL;
+                    tmp = NULL;
+                }
+            }   
+            
+            // History
             tmp2 = tmp;
         }
         while (tmp = tmp->next);
-        
-        
-        if (tmp && tmp2) {
-            // Remove value in middle
-            tmp2->next = tmp->next;
-        }
-        else if(!tmp2) {
-            // removing first value
-            head = tmp->next;
-        }
-        else {
-            // removing last value
-            tmp2->next = NULL;
-        }
-        delete tmp;
         size--;
     }
     
@@ -102,6 +107,9 @@ int main() {
     LinkedList lst;
     
     lst.insert(1);
+    lst.insert(1);
+    lst.insert(1);
+    lst.insert(1);
     lst.insert(2);
     lst.insert(3);
     lst.insert(4);
@@ -111,14 +119,14 @@ int main() {
     lst.insert(8);
     lst.insert(9);
     lst.insert(10);
-        
-    lst.remove(1);
-    lst.remove(2);
-    lst.remove(5);
+    
+    lst.remove(1);  // First element
+    lst.remove(2);  // from mid
+    lst.remove(5);  // from mid
     lst.remove(10);
     
-    lst.insertAfter(8/*key*/,20/*data*/);
-    lst.insertAfter(20,10);
+    lst.insertAfter(9, 10);   // It will be added after 9 not before because this model is based on LIFO
+    lst.insertAfter(10,11);
     
     cout << "Location of 9 in LinkedList: " << lst.search(9);
     
