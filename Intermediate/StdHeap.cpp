5677275d3047b3db0d6f91821022b5d693445f88
Owais <soac@outlook.com>
2015-12-13 22:59:04 +0500

    Minor fix

--- Intermediate/StdHeap.cpp
+++ Intermediate/StdHeap.cpp
@@ -4,14 +4,13 @@
 using namespace std;
 
 int main(){
-    vector<int> heap = {4,6,2,4,1,7,9,4,3,6,7,8,4,0};
+    vector<int> heap = {3,5,4,1,6,8,7,9,0,2};
     make_heap(heap.begin(), heap.end());
     while (!heap.empty()) {
-        pop_heap(heap.begin(), heap.end());
-        val = heap.front(); 
-        heap.pop_back();
+        int val = heap.front();
         cout << val << endl;
-        heap.pop_back();
+        pop_heap(heap.begin(), heap.end());
+        heap.pop_back();        
     }
     return 0;
 }
